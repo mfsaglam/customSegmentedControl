e5e6d0e67c6b4de79378d661ddaf525f2c86c26c
mfsaglam <mfsaglam1@gmail.com>
2021-03-24 21:18:47 +0300

    Initial Commit

---
+++ CustomSegmentedController/CustomSegmentedControl.swift
@@ -0,0 +1,98 @@
+//
+//  CustomSegmentedControl.swift
+//  CustomSegmentedController
+//
+//  Created by Fatih Sağlam on 24.03.2021.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomSegmentedControl: UIView {
+    
+    var buttons = [UIButton]()
+    var selector: UIView!
+    @IBInspectable
+    var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor = .clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var commaSeperatedButtonTitles: String = "" {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable
+    var buttonColor: UIColor = .systemGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable
+    var selectorColor: UIColor = .darkGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable
+    var selectorTextColor: UIColor = .white {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        buttons.removeAll()
+        subviews.forEach { $0.removeFromSuperview() }
+        let buttonTitles = commaSeperatedButtonTitles.components(separatedBy: ",")
+        for title in buttonTitles {
+            let button = UIButton(type: .system)
+            button.setTitle(title, for: .normal)
+            button.setTitleColor(buttonColor, for: .normal)
+            button.addTarget(self, action: #selector(buttonTapped(button:)), for: .touchUpInside)
+            buttons.append(button)
+        }
+        let selectorWidth = frame.width / CGFloat(buttons.count)
+        selector = UIView(frame: CGRect(x: 0, y: 0, width: selectorWidth, height: frame.height))
+        selector.layer.cornerRadius = selector.frame.height / 2
+        selector.backgroundColor = selectorColor
+        addSubview(selector)
+        
+        let sv = UIStackView(arrangedSubviews: buttons)
+        sv.axis = .horizontal
+        sv.alignment = .fill
+        sv.distribution = .fillProportionally
+        addSubview(sv)
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        sv.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        sv.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        sv.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+    }
+
+    override func draw(_ rect: CGRect) {
+        layer.cornerRadius = layer.frame.size.height / 2
+    }
+    
+    @objc func buttonTapped(button: UIButton) {
+        for btn in buttons {
+            btn.setTitleColor(buttonColor, for: .normal)
+            if btn == button {
+                btn.setTitleColor(selectorTextColor, for: .normal)
+            }
+        }
+    }
+}
